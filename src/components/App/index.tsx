84d3e7c4651a231b3a53674f9642febcd074958a
pon <my@maggit.ru>
2019-02-16 12:23:52 +0300

    Add scss and css modules

---
+++ src/components/App/index.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// const styles = require('./styles.module.css')
+import styles from './styles.module.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        app
+        <header className={styles.header}>
+          header
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
